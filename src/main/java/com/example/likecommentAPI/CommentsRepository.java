979394d9c7fe97a3b650e921863d3d92e2bcc9e1
gawrawa <gtjayawickrama@gmail.com>
2025-04-27 08:34:02 +0530

    Update @ CommentRepository.java

--- src/main/java/com/example/likecommentAPI/CommentsRepository.java
+++ src/main/java/com/example/likecommentAPI/CommentsRepository.java
@@ -1,9 +1,15 @@
 package com.example.likecommentAPI;
 
+import java.util.List;
+
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentsRepository extends MongoRepository<Comments, ObjectId> {
+
+    
+    Optional<Comments> findById(ObjectId id);
+    List<Comments> findByCourseId(String courseId);
 }
