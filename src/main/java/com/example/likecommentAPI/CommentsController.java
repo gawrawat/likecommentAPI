efeda70fe85485c74ab2349f713186a283b7048f
gawrawa <gtjayawickrama@gmail.com>
2025-04-27 10:30:36 +0530

    Update CommentsController.java

--- src/main/java/com/example/likecommentAPI/CommentsController.java
+++ src/main/java/com/example/likecommentAPI/CommentsController.java
@@ -1,6 +1,8 @@
 package com.example.likecommentAPI;
 
+import com.example.likecommentAPI.dto.NewCommentRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -20,16 +22,23 @@ public class CommentsController {
     }
 
     @GetMapping("/{id}")
-public ResponseEntity<?> getCommentById(@PathVariable String id) {
-    Map<String, Object> response = commentService.getCommentById(id);
-    if (response == null) {
-        return ResponseEntity.notFound().build();
+    public ResponseEntity<?> getCommentById(@PathVariable String id) {
+        Map<String, Object> response = commentService.getCommentById(id);
+        if (response == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(response);
     }
-    return ResponseEntity.ok(response);
-}
+
     @GetMapping("/course/{courseId}")
     public ResponseEntity<List<Map<String, Object>>> getCommentsByCourseId(@PathVariable String courseId) {
         List<Map<String, Object>> response = commentService.getCommentsByCourseId(courseId);
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<Comments> addComment(@RequestBody NewCommentRequest newCommentRequest) {
+        Comments savedComment = commentService.addNewComment(newCommentRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedComment);
     }
 }
