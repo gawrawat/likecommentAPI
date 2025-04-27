7a862fd1caef17f9ec9583e0af8f2c31e1494b0f
gawrawa <gtjayawickrama@gmail.com>
2025-04-27 08:37:59 +0530

    Edited commentcontroller.java

--- src/main/java/com/example/likecommentAPI/CommentsController.java
+++ src/main/java/com/example/likecommentAPI/CommentsController.java
@@ -3,6 +3,7 @@ package com.example.likecommentAPI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -19,5 +20,15 @@ public class CommentsController {
     public ResponseEntity<List<Map<String, Object>>> getAllComments() {
         List<Map<String, Object>> response = commentService.getAllCommentsWithParsedContent();
         return ResponseEntity.ok(response);
+    }
+
+        @GetMapping("/{id}")
+    public Comments getCommentById(@PathVariable String id) {
+        return commentService.getCommentById(id);
+    }
+
+    @GetMapping("/course/{courseId}")
+    public List<Comments> getCommentsByCourseId(@PathVariable String courseId) {
+        return commentService.getCommentsByCourseId(courseId);
     }
 }
--- src/main/java/com/example/likecommentAPI/CommentsRepository.java
+++ src/main/java/com/example/likecommentAPI/CommentsRepository.java
@@ -1,15 +1,14 @@
 package com.example.likecommentAPI;
 
-import java.util.List;
-
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface CommentsRepository extends MongoRepository<Comments, ObjectId> {
-
     
-    Optional<Comments> findById(ObjectId id);
     List<Comments> findByCourseId(String courseId);
+    
+
 }
